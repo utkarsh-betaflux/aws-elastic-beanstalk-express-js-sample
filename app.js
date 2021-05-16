1722e4274e34891fdb0f62ee43e6bc783d7cc22c
Utkarsh Sinha <utkarsh@betaflux.co>
2021-05-17 03:28:13 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Betafluxers!'));
+app.get('/', (req, res) => res.send('Hello Betafluxers! This is a full pipeline test.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
